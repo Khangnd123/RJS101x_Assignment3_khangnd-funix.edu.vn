76965ba1dc4234e439825e7facb3ea1afe02c951
khangnd123 <khangndfx16346@funix.edu.vn>
2022-11-12 13:35:50 +0700

    dasd

---
+++ src/components/Vidu.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Vidu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      a: "",
+    };
+  }
+}
+export default Vidu;
